a3375e0368e4ef81928c09bc2cb86e4c788831ab
SandraRadz <oleksandraradzievska@gmail.com>
2018-11-17 12:34:15 +0200

    all queries to db

--- Bank/src/main/java/dao/WriteOffDao.java
+++ Bank/src/main/java/dao/WriteOffDao.java
@@ -1,11 +1,4 @@
-package dao;
-
-import entity.WriteOff;
+package dao.impl;
 
 public interface WriteOffDao {
-    WriteOff get(long id);
-    int insert(WriteOff user);
-    void update(WriteOff user);
-    //void remove(WriteOff woff);
-    //boolean ifExists(WriteOff woff);
 }
