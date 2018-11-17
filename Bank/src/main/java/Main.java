3803de95de7e046d1e25de2b27a59f12415ae42c
SandraRadz <oleksandraradzievska@gmail.com>
2018-11-17 14:48:48 +0200

    main

--- Bank/src/main/java/Main.java
+++ Bank/src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] ar) {
         int port = 6666; // случайный порт (может быть любое число от 1025 до 65535)
-        CardServiceImpl cs = new CardServiceImpl();
+        /*CardServiceImpl cs = new CardServiceImpl();
         //cs.getCash("2222222222222223", "7777", -2000);
         System.out.println(cs.isCardExist("2222222222222223","7777"));
         System.out.println(cs.getSum("2222222222222223","7777"));
@@ -16,6 +16,8 @@ public class Main {
         System.out.println(cs.getSum("2222222222222222","7878"));
         boolean e= true;
         System.out.println(String.valueOf(e));
-
+*/
+        Controller controller = new Controller(port);
+        controller.start();
     }
 }
